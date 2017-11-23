b90b4969bc153c5d602a2c4e80457203e779d375
gdoucet <gdoucet@btssio-VirtualBox>
2017-11-23 17:04:00 +0100

    Création de la classe "Representation.java" (modele.metier)

--- src/modele/metier/Representation.java
+++ src/modele/metier/Representation.java
@@ -5,10 +5,47 @@
  */
 package modele.metier;
 
+import java.sql.Time;
+
 /**
  *
- * @author ydurand
+ * @author gdoucet
  */
 public class Representation {
     
+    private int id;
+    private String date;
+    private int id_lieu;
+    private String id_groupe;
+    private String heureDeb;
+    private String heureFin;
+    
+    public Representation(int id, String date_representation, int id_lieu, String id_groupe, String heureDebut_representation, String heureFin_representation) {
+        this.id = id;
+        this.date = date_representation;
+        this.id_lieu = id_lieu;
+        this.id_groupe = id_groupe;
+        this.heureDeb = heureDebut_representation;
+        this.heureFin = heureFin_representation;
+    }
+    
+    public int getIdRepresentation() {
+        return id;
+    }
+    public String getDateRepresentation() {
+        return date;
+    }
+    public int getIdLieu() {
+        return id_lieu;
+    }
+    public String getIdGroupe() {
+        return id_groupe;
+    }
+    public String getHeureDebRepresentation() {
+        return heureDeb;
+    }
+    public String getHeureFinRepresentation() {
+        return heureFin;
+    }
+        
 }
