aacb883448eb5e1de8937a262ccfc51f17a22a25
btssio <btssio@btssio-VirtualBox>
2017-12-07 16:39:19 +0100

    creation de la classe groupeDao, de la classe Groupe de la vueRepresentation et du controleur representation

--- src/controleur/CtrlRepresentation.java
+++ src/controleur/CtrlRepresentation.java
@@ -5,10 +5,96 @@
  */
 package controleur;
 
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import modele.dao.RepresentationDao;
+import modele.metier.Representation;
+import vue.vueRepresentation;
+
 /**
  *
  * @author ydurand
  */
-public class CtrlRepresentation {
+public class CtrlRepresentation implements WindowListener {
+    
+    private vueRepresentation vue;
+    
+    public CtrlRepresentation(vueRepresentation vue) throws SQLException {
+        this.vue = vue;
+        // le contrôleur écoute la vue
+        this.vue.addWindowListener(this);
+        List<Representation> lesRepresentations = null;
+        lesRepresentations = RepresentationDao.selectAll();
+               
+    }
+    
+    private void afficherRepresentation(List<Representation> desRepresentation) {
+        getVue().getModeleTableRepresentation().setRowCount(0);
+        String[] titresColonnes = {"Groupe", "lieu", "date", "heure Debut", "Heure Fin"};
+        getVue().getModeleTableRepresentation().setColumnIdentifiers(titresColonnes);
+        String[] ligneDonnees = new String[5];
+        for (Representation uneRepresentation : desRepresentation) {
+            ligneDonnees[0] = uneRepresentation.getIdGroupe();
+            ligneDonnees[1] = uneRepresentation.getIdLieu();
+            ligneDonnees[2] = uneRepresentation.getDateRepresentation();
+            ligneDonnees[3] = uneRepresentation.getHeureDebRepresentation();
+            ligneDonnees[4] = uneRepresentation.getHeureFinRepresentation();
+            getVue().getModeleTableRepresentation().addRow(ligneDonnees);
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    public vueRepresentation getVue() {
+        return vue;
+    }
+
+    public void setVue(vueRepresentation vue) {
+        this.vue = vue;
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- src/modele/metier/Groupe.java
+++ src/modele/metier/Groupe.java
@@ -11,4 +11,51 @@ package modele.metier;
  */
 public class Groupe {
     
+    private int id;
+    private String nom;
+    private String adressePostale;
+    private String identiteResponsable;
+    private String nbPersonne;
+    private String nomPays;
+    private String hebergement;
+    
+    public Groupe(int id, String nom, String adressePostale, String identiteResponsable, String nbPersonne, String nomPays, String hebergement) {
+    
+        this.id=id;
+        this.nom=nom;
+        this.adressePostale=adressePostale;
+        this.identiteResponsable=identiteResponsable;
+        this.nbPersonne=nbPersonne;
+        this.nomPays=nomPays;
+        this.hebergement=hebergement;
+}
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getAdressePostale() {
+        return adressePostale;
+    }
+
+    public String getIdentiteResponsable() {
+        return identiteResponsable;
+    }
+
+    public String getNbPersonne() {
+        return nbPersonne;
+    }
+
+    public String getNomPays() {
+        return nomPays;
+    }
+
+    public String getHebergement() {
+        return hebergement;
+    }
+    
 }
