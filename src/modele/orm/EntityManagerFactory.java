03b9c14186433538d380b90a1206455082324127
ydurand <ydurand@btssio-VirtualBox>
2018-03-06 14:26:39 +0100

    Création de la persistence et de l'entity manager

---
+++ src/modele/orm/EntityManagerFactory.java
@@ -0,0 +1,7 @@
+package modele.orm;
+import javax.persistence.Persistence;
+public class EntityManagerFactory {
+    public static void main(String[] args) {
+        Persistence.createEntityManagerFactory("BilletJava2017PU");
+    }   
+}
