c31544f49109817dab2a79c9f8feae0777b0d944
ydurand <ydurand@btssio-VirtualBox>
2018-03-24 13:40:52 +0100

    Ajout des colonnes Nom et Prénom des utilisateurs

--- src/sql/create_table_utilisateur.sql
+++ src/sql/create_table_utilisateur.sql
@@ -12,6 +12,8 @@
 
 CREATE TABLE `Utilisateur` (
   `idUtilisateur` int(11) NOT NULL,
+  `nom` varchar(40) NOT NULL,
+  `prenom` varchar(40) NOT NULL,
   `nomUtilisateur` varchar(40) NOT NULL,
   `motDePasse` varchar(40) NOT NULL
 );
@@ -20,13 +22,13 @@ CREATE TABLE `Utilisateur` (
 -- Contenu de la table `Utilisateur`
 --
 
-INSERT INTO `Utilisateur` (`idUtilisateur`, `nomUtilisateur`, `motDePasse`) VALUES
-('1', 'ydurand', '1234'),
-('2', 'mroux', '1234'),
-('3', 'dgregory', '1234'),
-('4', 'cgregory', '1234'),
-('5', 'nbourgeois', '1234'),
-('6', 'jpbeauvais', '1234');
+INSERT INTO `Utilisateur` (`idUtilisateur`, `nom`, `prenom`, `nomUtilisateur`, `motDePasse`) VALUES
+('1', 'Durand', 'Yann', 'ydurand', '1234'),
+('2', 'Roux', 'Maxime', 'mroux', '1234'),
+('3', 'Doucet', 'Gregory', 'dgregory', '1234'),
+('4', 'Cormier', 'Gregory', 'cgregory', '1234'),
+('5', 'Bourgeois', 'Nicolas', 'nbourgeois', '1234'),
+('6', 'Beauvais', 'Jean-Pierre', 'jpbeauvais', '1234');
 
 --
 -- Contrainte de la table `Utilisateur`
