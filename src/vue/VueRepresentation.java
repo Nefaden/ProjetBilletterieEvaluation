a8b1351b23d6af272dbb369281fd8c0fb3370099
ydurand <ydurand@btssio-VirtualBox>
2018-03-25 22:54:51 +0200

    Ajout d'une méthode pour pouvoir quitter la vue

--- src/vue/VueRepresentation.java
+++ src/vue/VueRepresentation.java
@@ -39,6 +39,14 @@ public class VueRepresentation extends VueGenerique {
     }
     
     /**
+     * 
+     * @return JButton jButtonQuitter : Getter du bouton quitter
+     */
+    public JButton getjButtonQuitter() {
+        return jButtonQuitter;
+    }
+    
+    /**
      *  
      * @return JTable jTableRepresentation : Getter du jTable de la liste des representations
      */
@@ -58,7 +66,7 @@ public class VueRepresentation extends VueGenerique {
         jLabelTitre = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTableRepresentation = new javax.swing.JTable();
-        jButtonquitter = new javax.swing.JButton();
+        jButtonQuitter = new javax.swing.JButton();
         jButtonReserver = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -86,10 +94,10 @@ public class VueRepresentation extends VueGenerique {
         });
         jScrollPane1.setViewportView(jTableRepresentation);
 
-        jButtonquitter.setText("quitter");
-        jButtonquitter.addActionListener(new java.awt.event.ActionListener() {
+        jButtonQuitter.setText("quitter");
+        jButtonQuitter.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonquitterActionPerformed(evt);
+                jButtonQuitterActionPerformed(evt);
             }
         });
 
@@ -105,10 +113,10 @@ public class VueRepresentation extends VueGenerique {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButtonquitter)
-                .addGap(18, 18, 18)
                 .addComponent(jButtonReserver)
-                .addGap(92, 92, 92))
+                .addGap(18, 18, 18)
+                .addComponent(jButtonQuitter)
+                .addGap(41, 41, 41))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 553, Short.MAX_VALUE)
@@ -123,7 +131,7 @@ public class VueRepresentation extends VueGenerique {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 307, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(31, 31, 31)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButtonquitter)
+                    .addComponent(jButtonQuitter)
                     .addComponent(jButtonReserver))
                 .addContainerGap(71, Short.MAX_VALUE))
         );
@@ -131,9 +139,9 @@ public class VueRepresentation extends VueGenerique {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButtonquitterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonquitterActionPerformed
+    private void jButtonQuitterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonQuitterActionPerformed
        dispose();
-    }//GEN-LAST:event_jButtonquitterActionPerformed
+    }//GEN-LAST:event_jButtonQuitterActionPerformed
 
     /**
      * @param args the command line arguments
@@ -172,8 +180,8 @@ public class VueRepresentation extends VueGenerique {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButtonQuitter;
     private javax.swing.JButton jButtonReserver;
-    private javax.swing.JButton jButtonquitter;
     private javax.swing.JLabel jLabelTitre;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTableRepresentation;
