89367bd6480a491e03ae396c3fd63d8c797e9296
ydurand <ydurand@btssio-VirtualBox>
2018-03-25 23:01:20 +0200

    Modification de la méthode afficherUneRepresentation avec une méthode de vérification de la valeur de la ligne et les actions liées aux boutons de la vue pour la naviguation

--- src/controleur/CtrlRepresentation.java
+++ src/controleur/CtrlRepresentation.java
@@ -12,12 +12,8 @@ import javax.swing.JOptionPane;
 import modele.dao.RepresentationDao;
 import modele.metier.Representation;
 import vue.VueRepresentation;
-import controleur.CtrlUneRepresentation;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JTable;
-import vue.VueUneRepresentation;
 
 /**
  * Contrôleur permettant l'affichage et la sélection des représentation dans la
@@ -30,31 +26,43 @@ public class CtrlRepresentation extends ControleurGenerique implements ActionLis
     private final RepresentationDao RepresentationDao = new RepresentationDao();
     private List<Representation> lesRepresentations;
 
+    // Constructeur du controller des représentations
     public CtrlRepresentation(CtrlPrincipal ctrlPrincipal) throws SQLException {
         super(ctrlPrincipal);
         vue = new VueRepresentation();
         afficherRepresentation();
         this.getVue().getjTableRepresentation().addMouseListener(this);
         this.getVue().getjButtonReservation().addActionListener(this);
+        this.getVue().getjButtonQuitter().addActionListener(this);
         vue.addWindowListener(this);
     }
-    
-    
-    /*
-    * Methode pour quitter l'application
-    */
+
+    /**
+     *
+     * Methode pour quitter l'application
+     *
+     * @throws SQLException
+     */
     public void menuFichierQuitter() throws SQLException {
         this.getCtrlPrincipal().action(EnumAction.MENU_QUITTER);
-    }    
-    
-    /*
-    * Methode pour quitter la vue des représentation    
-    */
+    }
+
+    /**
+     *
+     * Methode pour quitter la vue des représentation
+     *
+     * @throws SQLException
+     */
     public void representationQuitter() throws SQLException {
         this.getCtrlPrincipal().action(EnumAction.REPRESENTATION_QUITTER);
     }
 
-    //méthode pour afficher la liste des représentation via la methode sel
+    /**
+     *
+     * Méthode pour afficher les representation dans le jTable
+     *
+     * @throws SQLException
+     */
     public void afficherRepresentation() throws SQLException {
         String msg = ""; // message à afficher en cas d'erreur/
         ((VueRepresentation) vue).getModeleTableRepresentation().setRowCount(0);
@@ -71,42 +79,66 @@ public class CtrlRepresentation extends ControleurGenerique implements ActionLis
                 ligneDonnees[4] = uneRepresentation.getHeureDebutRepresentation();
                 ligneDonnees[5] = uneRepresentation.getHeureFinRepresentation();
                 ((VueRepresentation) vue).getModeleTableRepresentation().addRow(ligneDonnees);
-            } 
+            }
         } catch (SQLException ex) {
             msg = "CtrlRepresentation - representationAfficher() - " + ex.getMessage();
             JOptionPane.showMessageDialog(vue, msg, "Affichage des représentations", JOptionPane.ERROR_MESSAGE);
         }
     }
-    
-    public void afficherUneRepresentation() throws SQLException {
-        VueUneRepresentation uneVue = new VueUneRepresentation();
-        CtrlUneRepresentation ctrlUneRepresentation = new CtrlUneRepresentation(uneVue, 4);
-        this.getVue().setEnabled(false);
-        ctrlUneRepresentation.getVue().setVisible(true);
-    }
-    
-    
-    //int ligneSelectionne = lesRepresentations.getSelectedRow();
-    //on récupére la valeur de la première colonne de la ligne sélectionné
-    //taJTable.getValueAt(ligneSelectionne, 0);
-    
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        JTable lesRepresentations = this.getVue().getjTableRepresentation();
-        if (e.getSource().equals(getVue().getjButtonReservation())) {
-            try {
-                afficherUneRepresentation();
-            } catch (SQLException ex) {
-                Logger.getLogger(CtrlMenu.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            
-            if (e.getSource().equals(lesRepresentations)) {
-                int ligneSelectionne = lesRepresentations.getSelectedRow();
-                lesRepresentations.getValueAt(ligneSelectionne, 0);
-            }
+
+    public void afficherUneRepresentation() throws SQLException{
+        int ligne = ((VueRepresentation) vue).getjTableRepresentation().getSelectedRow();
+        int colonne = ((VueRepresentation) vue).getjTableRepresentation().getSelectedColumn();
+        if (ligne != -1 && colonne != -1){
+            String groupe = (String) ((VueRepresentation) vue).getModeleTableRepresentation().getValueAt(ligne,colonne);
+            this.getCtrlPrincipal().action(EnumAction.REPRESENTATION_DETAILS,groupe);
         }
     }
     
+    /**
+     *
+     * @param e : Evènement lié à la sélection d'une ligne dans le jTable
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(((VueRepresentation) vue).getjButtonQuitter())) {
+            try {
+                representationQuitter();
+            } catch (SQLException ex) {
+                Logger.getLogger(CtrlRepresentation.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.print("BOUTTON QUITTER");
+        } else {
+            if (e.getSource().equals(((VueRepresentation) vue).getjButtonReservation())) {
+                if (!verifierLignejTable()) {
+                    JOptionPane.showMessageDialog(null, "Représentation non sélectionnée.", "Inane error", JOptionPane.ERROR_MESSAGE);
+                } else {
+                    try {
+                        afficherUneRepresentation();
+                    } catch (SQLException ex) {
+                        Logger.getLogger(CtrlRepresentation.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean verifierLignejTable() {
+        boolean test;
+        int ligne = ((VueRepresentation) vue).getjTableRepresentation().getSelectedRow();
+        int colonne = ((VueRepresentation) vue).getjTableRepresentation().getSelectedColumn();
+        if (ligne != -1 && colonne != -1) {
+            test = true;
+        } else {
+            test = false;
+        }
+        return test;
+    }
+
+    /**
+     *
+     * @return VueRepresentation vue : Getter de la vue ds représentations
+     */
     @Override
     public VueRepresentation getVue() {
         return (VueRepresentation) vue;
